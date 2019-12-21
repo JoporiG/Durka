3541722a5ff402a6df511bbd84b9989a56a036a8
ABC123-crypto <mishkavyun2@gmail.com>
2019-12-21 15:11:21 +0300

    FST commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,64 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Cont>
+      <ChatWindow @send-message="sendMessage">
+        <ChatMessage v-for="message in data" :key="message.id" :username="message.author" :datetime="message.text"></ChatMessage>
+      </ChatWindow>>
+    </Cont>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+import Cont from './components/Container.vue'
+import ChatMessage from './components/ChatMessage.vue'
+import ChatWindow from './components/ChatWindow.vue'
 
 export default {
+  data(){
+    return { 
+      data: []
+    };
+  },
+  methods:{
+    sendMessage({nickname, message}){
+      axios.post('http://188.225.47.187/api/chat/sendmessage.php', {
+        author: nickname,
+        text: message,
+      }).then(response =>{
+        console.log(response);
+        this.getMessages();
+      })
+    },
+    getMessages(){
+      axios.get('http://188.225.47.187/api/chat/getmessages.php')
+      .then((response) => {
+          this.data = response.data;
+          //заполнить дату
+      });
+    },
+  },
   name: 'app',
   components: {
-    HelloWorld
+    Cont, ChatMessage, ChatWindow,
+  },
+  mounted() {
+    setInterval(()=>{
+      this.getMessages();
+    }, 3000)
+  
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.container {
+  width: 764px;
+  margin: auto;
+  box-shadow: 0 0 15px 0 #5f5f5f73;
+}
+body {
+  margin: 0;
+  background-color: #f9f9fa;
 }
 </style>
